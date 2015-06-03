0dbba4af9a31cc87aeed88e0bf189d40828258bb
Christos Erotocritou <kemimeister@gmail.com>
2015-06-04 00:39:31 +0100

    remote jar location

--- xap-scripts/install-xap.sh
+++ xap-scripts/install-xap.sh
@@ -68,13 +68,13 @@ if [ ! -d $DIR/xap ]; then
 
   # add dynamic nat mapper (needed for gateway)
 
-  ctx download-resource "xap-scripts/nat-mapper.jar" "@{\"target_path\": \"$GSDIR/lib/platform/ext/nat-mapper.jar\"}"
+  wget https://github.com/kemiz/xap-4-cloudify-3/raw/3.2m8/xap-scripts/nat-mapper.jar -P $GSDIR/lib/platform/ext/nat-mapper.jar
 
 
 else
   if [ ! -d /tmp/gsdir ]; then
       GSDIR=`ls -d $DIR/xap/gigaspaces*premium*`
       echo $GSDIR > /tmp/gsdir
-      ctx download-resource "xap-scripts/nat-mapper.jar" "@{\"target_path\": \"$GSDIR/lib/platform/ext/nat-mapper.jar\"}"
+      wget https://github.com/kemiz/xap-4-cloudify-3/raw/3.2m8/xap-scripts/nat-mapper.jar -P $GSDIR/lib/platform/ext/nat-mapper.jar
   fi
 fi
